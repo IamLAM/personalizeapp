cb535b186efff58604ca4cbf9603ce9bb07bf18b
Luis Angel Monge <ingeniero@luismonge.com.mx>
2024-06-17 14:44:40 -0700

    Version personalizada de  mi propia App

---
+++ android/app/src/main/kotlin/com/example/personalizeapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.personalizeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(
+      MaterialApp(
+        home: Scaffold(
+          title: Text(""),
+          appBar: AppBar(backgroundColor: Colors.amber),
+        ),
+      ),
+    ),
+  );
+}
