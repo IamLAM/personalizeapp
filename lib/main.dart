a6ea52ede8512ce53f231f8530b86867158795a3
Luis Angel Monge <ingeniero@luismonge.com.mx>
2024-06-17 15:34:10 -0700

    Version personalizada de  mi propia App

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MyApp(
-      MaterialApp(
-        home: Scaffold(
-          title: Text(""),
-          appBar: AppBar(backgroundColor: Colors.amber),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hi"),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Image.network("https://picsum.photos/250?image=9"),
         ),
       ),
     ),
